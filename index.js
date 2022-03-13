680a67958bac77a1dc63d3c6d05a9806efd81ace
becket <becket.bowes@gmail.com>
2022-03-13 00:25:35 -0500

    all pieces functioning

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 const cMInterface = require('./lib/inq');
 
 cMInterface();
-
-// const ask = require('./db/connect');
-// const cTable = require('console.table');
-// const db = require('./db/connect');
-
-// const getAll = function (sql) {
-//         db.query(sql, function (err, res, fields) {
-//             if (err) console.log(err);
-//             console.table(res);
-//             // console.log(fields);
-//         });
-// };
-
-// getAll(`SELECT * FROM role`);
