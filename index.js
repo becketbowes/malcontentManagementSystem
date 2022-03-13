afe4fbaf76cf2168f50403a1591b825d496211ba
becket <becket.bowes@gmail.com>
2022-03-13 01:17:09 -0500

    debugging

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const cMInterface = require('./lib/inq');
+const figlet = require('figlet')
+
+figlet('malcontentManagement', function(err, data) {
+    if (err) {
+        console.log('something went wrong');
+        console.dir(err);
+        return;
+    }
+    console.log('\n');
+    console.log(data);
+});
 
 cMInterface();
